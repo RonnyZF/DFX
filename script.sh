89052a6ee92cb08dd1e1c92c0f7b4a454f4e987d
ronnyzf <ronnyzarate@gmail.com>
2021-11-09 11:37:13 -0600

    script

---
+++ script.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+export SNPSLMD_LICENSE_FILE=27000@172.21.9.211
+export VCS_HOME=/home/gcastrolocal/tools/vcs/R-2020.12-SP2/
+export SYNOPSYS=/home/gcastrolocal/tools/icc/R-2020.09-SP5-1
+
